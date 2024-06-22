f895dcde4b5f232970fd1fc63d07cc1741d4127b
Jeremy Walton <jeremy.patrick.walton@gmail.com>
2024-06-22 16:55:16 -0400

    Display memories on profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
       @be_real_connection = @user.be_real_connection
       @be_real_person = @be_real_connection.person_record
       @be_real_friend_collection = @be_real_connection.friends
+      @be_real_memory_collection = @be_real_connection.memories
     end
   end
 end
