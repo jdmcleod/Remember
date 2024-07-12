ec06310f987231414aa665bdfe5e3e2044fad989
Josh McLeod <josh@mcleodfamily.org>
2024-07-12 09:08:12 -0400

    Add forward and back links

--- app/controllers/years_controller.rb
+++ app/controllers/years_controller.rb
@@ -2,5 +2,12 @@ class YearsController < ApplicationController
   def current
     @year = current_user.years.includes(quarters: [{ months: [{ days: :short_entry }] }]).current_year
     @year.generate_data if @year.missing_data?
+    render :show
+  end
+
+  def show
+    year = params[:id]
+    @year = current_user.years.includes(quarters: [{ months: [{ days: :short_entry }] }]).find_or_create_by(year:)
+    @year.generate_data if @year.missing_data?
   end
 end
