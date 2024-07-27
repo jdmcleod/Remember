0173653ba7f55d0385de51fe6f70a85f34309a54
Connor Torrell <connortorrell@gmail.com>
2024-07-26 22:21:05 -0400

    add crud for badges

--- app/controllers/badges_controller.rb
+++ app/controllers/badges_controller.rb
@@ -9,6 +9,49 @@ class BadgesController < ApplicationController
   end
 
   def edit
-    @badge = Badge.find(params[:id])
+    @badge = current_user.badges.find(params[:id])
+  end
+
+  def create
+    @badge = current_user.badges.build(badge_params)
+
+    if @badge.save
+      update_view_for_success
+    else
+      render turbo_stream: turbo_stream.update('modal-body', partial: 'badges/form')
+    end
+  end
+
+  def update
+    @badge = current_user.badges.find(params[:id])
+
+    if @badge.update(badge_params)
+      update_view_for_success
+    else
+      render turbo_stream: turbo_stream.update('modal-body', partial: 'badges/form')
+    end
+  end
+
+  def destroy
+    @badge = current_user.badges.find(params[:id])
+    @badge.destroy
+    respond_to do |format|
+      format.turbo_stream { update_view_for_success }
+      format.html { redirect_to badges_path }
+    end
+  end
+
+  private
+
+  def update_view_for_success
+    @badges = current_user.badges.order(created_at: :desc)
+
+    render turbo_stream: [
+      turbo_stream.update('modal-body', partial: 'badges/badges'),
+    ].join
+  end
+
+  def badge_params
+    params.require(:badge).permit(:name, :color, :icon_name)
   end
 end
