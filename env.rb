cb530eb902ab157ac63118e81b90bd112ff85ac8
Josh McLeod <josh@mcleodfamily.org>
2024-08-24 07:13:49 -0400

    Configure S3

--- env.rb
+++ env.rb
@@ -6,3 +6,7 @@ ENV['RAILS_ENV'] ||= 'development'
 # OAuth2 Google Authentication
 ENV["GOOGLE_CLIENT_ID"] ||= ''
 ENV["GOOGLE_CLIENT_SECRET"] ||= ''
+
+ENV["AWS_ACCESS_KEY_ID"] ||= ''
+ENV["AWS_SECRET_ACCESS_KEY"] ||= ''
+ENV["BUCKET_NAME"] ||= ''
