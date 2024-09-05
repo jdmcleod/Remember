fa08cd951cbc207b142a2d2d710c651131a6fcbc
Josh McLeod <josh@mcleodfamily.org>
2024-09-05 18:11:55 -0400

    WIP: Month entry

--- app/controllers/months_controller.rb
+++ app/controllers/months_controller.rb
@@ -4,4 +4,22 @@ class MonthsController < ApplicationController
     @entries = current_user.entries.in_range(@month.start_date, @month.end_date)
     render layout: 'modal'
   end
+
+  def entry_form
+    @month = current_user.months.find(params[:id])
+    @entry = @month.find_entry
+    render layout: 'modal'
+  end
+
+  def update_entries
+    @month = current_user.months.find(params[:id])
+    binding.pry
+    @month.update(month_entry_params)
+  end
+
+  private
+
+  def month_entry_params
+    params.require(:entry).permit(:content)
+  end
 end
