a8a010c67781ad7b481c27d8c37000423a6b0c5d
Connor Torrell <connortorrell@gmail.com>
2024-09-12 23:26:09 -0400

    sort days and such

--- app/models/quarter.rb
+++ app/models/quarter.rb
@@ -2,6 +2,6 @@ class Quarter < ApplicationRecord
   belongs_to :year
 
   has_one :user, through: :year
-  has_many :months, dependent: :destroy
+  has_many :months, -> { order(created_at: :asc) }, dependent: :destroy
   has_many :days, through: :months
 end
--- app/models/year.rb
+++ app/models/year.rb
@@ -1,7 +1,7 @@
 class Year < ApplicationRecord
   belongs_to :user
 
-  has_many :quarters, dependent: :destroy
+  has_many :quarters, -> { order(created_at: :asc) }, dependent: :destroy
   has_many :months, through: :quarters
   has_many :days, through: :months
 
