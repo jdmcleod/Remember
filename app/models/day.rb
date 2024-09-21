ef7a88ce6b302379f0b0f80bec17790a2ee2f501
Connor Torrell <connortorrell@gmail.com>
2024-09-21 16:51:18 -0400

    fix height of day

--- app/models/day.rb
+++ app/models/day.rb
@@ -12,7 +12,7 @@ class Day < ApplicationRecord
   has_many :be_real_memories
 
   has_one_attached :image do |attachable|
-    attachable.variant :thumb, resize_to_limit: [40, 40]
+    attachable.variant :thumb, resize_to_limit: [54, 54]
   end
 
   def self.on(date)
