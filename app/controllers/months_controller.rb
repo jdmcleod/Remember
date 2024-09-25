88ec4e20bb9b0adc4d35139ff2d9865b1e61273b
Josh McLeod <josh@mcleodfamily.org>
2024-09-25 09:47:42 -0400

    Add photos to events show modal

--- app/controllers/months_controller.rb
+++ app/controllers/months_controller.rb
@@ -2,7 +2,7 @@ class MonthsController < ApplicationController
   def show
     @month = current_user.months.find(params[:id])
     @entries = current_user.entries.in_range(@month.start_date, @month.end_date)
-    render layout: 'modal',  locals: { modal_class: 'modal--full' }
+    render layout: 'modal', locals: { modal_class: 'modal--full' }
   end
 
   def entry_form
