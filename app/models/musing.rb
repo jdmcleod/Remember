36196a54ebdf474d8ce47ef5be1b724139deffd3
Connor Torrell <connortorrell@gmail.com>
2024-09-26 14:16:20 -0400

    add types

--- app/models/musing.rb
+++ app/models/musing.rb
@@ -2,4 +2,6 @@ class Musing < ApplicationRecord
   belongs_to :day
 
   has_one :entry, as: :journalable, dependent: :destroy, class_name: 'Entry'
+
+  enum type: [ :book, :movie, :album, :bible_verse, :realization, :perplexity, :resolution, :testimony ]
 end
