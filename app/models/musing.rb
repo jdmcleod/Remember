7ce0e5a3774d235df1be64a0f6e062e4d1f77007
Connor Torrell <connortorrell@gmail.com>
2024-09-26 14:09:43 -0400

    change to belongs to day only

--- app/models/musing.rb
+++ app/models/musing.rb
@@ -1,9 +1,5 @@
 class Musing < ApplicationRecord
   belongs_to :day
-  belongs_to :month, through: :day
-  belongs_to :quarter, through: :month
-  belongs_to :year, through: :quarter
-  belongs_to :user, through: :year
 
   has_one :entry, as: :journalable, dependent: :destroy, class_name: 'Entry'
 end
