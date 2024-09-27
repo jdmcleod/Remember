679af5abfafba4a769ddcc36a19b12fdecb894fd
Connor Torrell <connortorrell@gmail.com>
2024-09-26 22:49:36 -0400

    add controller

---
+++ app/controllers/musings_controller.rb
@@ -0,0 +1,45 @@
+class MusingsController < ApplicationController
+  before_action :set_day
+
+  def new
+    @musing = @day.musings.new
+  end
+
+  def edit
+    @musing = Musing.find(params[:id])
+  end
+
+  def update
+    @musing = Musing.find(params[:id])
+
+    if @musing.update(musing_params)
+      # turbo
+    end
+  end
+
+  def create
+    @musing = @day.musings.build(musing_params)
+
+    if @musing.save
+      # turbo
+    end
+  end
+
+  def destroy
+    @musing = Musing.find(params[:id])
+
+    if @musing.destroy
+      # turbo
+    end
+  end
+
+  private
+
+  def set_day
+    @day = Day.find(params[:day_id])
+  end
+
+  def musing_params
+    params.require(:musing).permit(:name, :description, :type)
+  end
+end
