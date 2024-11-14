8981d49519d0bc14a34d7aca4469ca9f89794d77
Josh McLeod <josh@mcleodfamily.org>
2024-11-13 20:49:36 -0500

    Don't show monthly entry in month show

--- app/controllers/months_controller.rb
+++ app/controllers/months_controller.rb
@@ -1,7 +1,7 @@
 class MonthsController < ApplicationController
   def show
     @month = current_user.months.find(params[:id])
-    @entries = current_user.entries.in_range(@month.start_date, @month.end_date)
+    @entries = current_user.entries.where(journalable_type: 'Day').in_range(@month.start_date, @month.end_date)
     render layout: 'modal', locals: { modal_class: 'modal--full' }
   end
 
