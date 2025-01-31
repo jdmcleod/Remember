331799af202695b21573b8a7e8163b51d850b2de
Josh McLeod <josh@mcleodfamily.org>
2025-01-31 17:24:42 -0500

    Add PWA

--- config/routes.rb
+++ config/routes.rb
@@ -52,4 +52,8 @@ Rails.application.routes.draw do
   resources :admin, only: [:index]
 
   root to: 'years#current'
+
+  # Render dynamic PWA files from app/views/pwa/*
+  get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+  get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 end
