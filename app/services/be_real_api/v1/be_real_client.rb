5b397586a01e56b16336baaa8ed8250913380d5f
Joshua McLeod <44909026+jdmcleod@users.noreply.github.com>
2025-02-22 12:32:16 -0500

    Fix BeReal api endpoint and day popup switcher (#38)
    * Fix search for date bug
    
    * Floating text around image
    
    * Fix next day button
    
    * Update to new secret url

--- app/services/be_real_api/v1/be_real_client.rb
+++ app/services/be_real_api/v1/be_real_client.rb
@@ -3,7 +3,7 @@
 module BeRealApi
   module V1
     class BeRealClient
-      API_ENDPOINT = 'https://mobile.bereal.com/api'.freeze
+      API_ENDPOINT = 'https://mobile-l7.bereal.com/api'.freeze
       AUTH_ENDPOINT = 'https://auth.bereal.team/token?grant_type=firebase'.freeze
       APP_TOKEN = '54F80A258C35A916B38A3AD83CA5DDD48A44BFE2461F90831E0F97EBA4BB2EC7'.freeze
       CLIENT_SECRET = '962D357B-B134-4AB6-8F53-BEA2B7255420'.freeze
