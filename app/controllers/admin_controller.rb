bbd382d55061eddce2de3196e6462e35ecf9d8d6
Josh McLeod <josh@mcleodfamily.org>
2025-02-24 22:12:59 -0500

    more bug fixes

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,7 +2,7 @@ class AdminController < ApplicationController
   before_action :ensure_admin!
 
   def index
-    @users = User.includes(:entries, :badges, :events).order(:name)
+    @users = User.left_joins(:entries).includes(:badges, :events).order('COUNT(entries.id) DESC').group('users.id')
 
     @entries_per_day = Entry
       .where
