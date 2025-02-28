842b68c03df897940414ba5a9428ecf59e1be758
Josh McLeod <josh@mcleodfamily.org>
2025-02-27 21:34:12 -0500

    Click outside and more view updates

--- db/migrate/20250223203114_change_musing_type_to_kind.rb
+++ db/migrate/20250223203114_change_musing_type_to_kind.rb
@@ -4,7 +4,8 @@ class ChangeMusingTypeToKind < ActiveRecord::Migration[8.0]
     add_column :musings, :user_id, :bigint
     add_foreign_key :musings, :users
 
-    remove_column :musings, :day_id
+    remove_column :musings, :day_id, :bigint
     add_column :musings, :date, :datetime
+    remove_column :musings, :description, :string
   end
 end
